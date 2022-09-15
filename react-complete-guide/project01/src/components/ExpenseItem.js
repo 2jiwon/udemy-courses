c9212b12d410705cb2c231edbeb6c781ac7b255c
이지원 <ljwjulian@gmail.com>
2022-09-15 11:31:02 +0900

    컴포넌트에 props를 통해 데이터 전달하고 출력하기

--- react-complete-guide/project01/src/components/ExpenseItem.js
+++ react-complete-guide/project01/src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
     return (
         <div className='expense-item'>
-            <div>Date</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>Title</h2>
             </div>
