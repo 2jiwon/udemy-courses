6b6691bb881f208b05f8b8b5755cbfbca29c8358
이지원 <ljwjulian@gmail.com>
2022-09-22 18:10:06 +0900

    과제2 > ExpensesFilter 적용

--- react-complete-guide/project01/src/components/Expenses/Expenses.js
+++ react-complete-guide/project01/src/components/Expenses/Expenses.js
@@ -1,12 +1,25 @@
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+    const [filteredData, setFilteredData] = useState(props.data);
+    const filterSelectHandler = (selectedData) => {
+        setFilteredData(props.data);
+        if (selectedData !== "") {
+            setFilteredData(props.data.filter((d) =>
+                d.date.getFullYear().toString() === selectedData
+            ));
+        }
+    }
+
     return (
         <Card className="expenses">
+            <ExpensesFilter onFilterSelected={filterSelectHandler} />
             {
-                props.data.map((e) =>
+                filteredData.map((e) =>
                     <ExpenseItem title={e.title} amount={e.amount} date={e.date}></ExpenseItem>
                 )
             }
