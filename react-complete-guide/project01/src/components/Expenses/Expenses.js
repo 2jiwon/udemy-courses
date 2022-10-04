d1a1a9226a0fd328cd4eb65840d49e0b61b4ffbd
이지원 <ljwjulian@gmail.com>
2022-10-04 16:00:54 +0900

    필터링과 메시지 출력을 변수를 이용해서 간단하게 수정

--- react-complete-guide/project01/src/components/Expenses/Expenses.js
+++ react-complete-guide/project01/src/components/Expenses/Expenses.js
@@ -10,18 +10,21 @@ const Expenses = (props) => {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     }
-
     const filteredExpenses = props.items.filter(expense => {
         return (filteredYear === '') ? expense : expense.date.getFullYear().toString() === filteredYear;
     });
 
+    let expensesContent = <p>No expenses found.</p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.length > 0 && filteredExpenses.map((e) =>
+            <ExpenseItem key={e.id} title={e.title} amount={e.amount} date={e.date}></ExpenseItem>
+        );
+    }
+
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-            {filteredExpenses.length > 0 && filteredExpenses.map((e) =>
-                <ExpenseItem key={e.id} title={e.title} amount={e.amount} date={e.date}></ExpenseItem>
-            )}
+            {expensesContent}
         </Card>
     );
 }
