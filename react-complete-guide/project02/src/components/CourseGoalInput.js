ec4c9ac3740137ee68843acf132acd6766482ee2
이지원 <ljwjulian@gmail.com>
2022-11-04 17:03:46 +0900

    새로 입력한 goal을 처리하는 부분 추가

--- react-complete-guide/project02/src/components/CourseGoalInput.js
+++ react-complete-guide/project02/src/components/CourseGoalInput.js
@@ -1,11 +1,24 @@
 import './CourseGoalInput.css';
+import { useState } from 'react';
 import Button from './UI/Button/Button';
 
 const CourseGoalInput = (props) => {
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const goalInputChangeHandler = event => {
+        // console.log(event.target.value);
+        setEnteredValue(event.target.value);
+    }
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        console.log(enteredValue);
+    }
+
     return (
-        <form onSubmit="">
-            <div class="form-control">
-                <input type="text" placeholder='Enter your goal' />
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label>Course Goal</label>
+                <input type="text" placeholder='Enter your goal' onChange={goalInputChangeHandler} />
             </div>
             <Button type="submit">Add Goal</Button>
         </form>
