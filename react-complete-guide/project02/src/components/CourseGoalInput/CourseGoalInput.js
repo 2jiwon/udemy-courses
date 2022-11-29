c4d8c87efb6dada89877f0decf8f46fe21e97fc1
이지원 <ljwjulian@gmail.com>
2022-11-29 18:08:42 +0900

    폼 전송후에 input 초기화하는 부분 추가

--- react-complete-guide/project02/src/components/CourseGoalInput/CourseGoalInput.js
+++ react-complete-guide/project02/src/components/CourseGoalInput/CourseGoalInput.js
@@ -1,25 +1,27 @@
 import './CourseGoalInput.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Button from '../UI/Button/Button';
 
 const CourseGoalInput = (props) => {
     const [enteredValue, setEnteredValue] = useState('');
+    const inputRef = useRef();
 
     const goalInputChangeHandler = event => {
-        // console.log(event.target.value);
+        // console.log(event.target.value); // 글자 입력할 때 마다 출력됨
         setEnteredValue(event.target.value);
     }
     const formSubmitHandler = event => {
         event.preventDefault();
         // console.log(enteredValue);
         props.onAddGoal(enteredValue);
+        inputRef.current.value = ""; // 폼 전송 후에 input 초기화
     }
 
     return (
         <form onSubmit={formSubmitHandler}>
             <div className="form-control">
                 <label>Course Goal</label>
-                <input type="text" placeholder='Enter your goal' onChange={goalInputChangeHandler} />
+                <input type="text" placeholder='Enter your goal' onChange={goalInputChangeHandler} ref={inputRef} />
             </div>
             <Button type="submit">Add Goal</Button>
         </form>
